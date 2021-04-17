b5522d7449e584f1feb7df1f0defab5727b6e26f
Endersky78 <endersky79@gmail.com>
2021-04-16 19:20:31 -0700

    added basic player movement

---
+++ Assets/Scripts/Player Scripts/Mouse_Look.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse_Look : MonoBehaviour {
+    
+    public float mouseSensitivity;
+    public Transform playerTransform;
+
+    float xRotation = 0f;
+
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update() {
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+         // Gets mouse Y input as a rotation and the clamps it so player doesn't look upside down
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerTransform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/Player Scripts/Player_Movement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class Player_Movement : MonoBehaviour {
+    [Header("Movement Values")]
+    public float playerSpeed;
+    public float sprintMult; /*do I want this?*/
+    public float gravity;
+    public float jumpHeight;
+    public bool isGrounded;
+    public bool isSprinting;
+
+    [Header("Components")]
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
+    private float groundDistance = 0.4f;
+
+    private CharacterController controller;
+    private Vector3 moveDirec = Vector3.zero;
+    private Vector3 velocity;
+
+    void Start() {
+        controller = GetComponent<CharacterController>();
+    }
+
+
+    void Update() {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        isSprinting = Input.GetButton("Sprint");
+
+        float horizontal = Input.GetAxis("Horizontal");  
+        float vertical = Input.GetAxis("Vertical");
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        moveDirec = transform.right * horizontal + transform.forward * vertical;
+
+        velocity.y += gravity * Time.deltaTime;
+
+        if (moveDirec != Vector3.zero && isSprinting) {
+            controller.Move(moveDirec * Time.deltaTime * playerSpeed * sprintMult);
+        } else if (moveDirec != Vector3.zero) {
+            controller.Move(moveDirec * Time.deltaTime * playerSpeed);
+        }
+        
+        // Done this way to replicate actual real world physics
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void OnGUI() {
+         GUILayout.Label("Grounded: " + isGrounded);
+    }
+}
